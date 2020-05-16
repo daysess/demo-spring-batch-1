269302b895ac985dfcb08f41cdbc594df0da7921
Dayse Soares <daysedesign@gmail.com>
2020-05-16 18:57:05 -0300

    Projeto spring batch

---
+++ src/main/java/br/com/daysesoares/DemoSpringBatch1Application.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBatch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daysesoares/DemoSpringBatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
